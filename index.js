73019b574cd1998e1cad783d6f103b0b310df027
michael valiahmetov <michael.valiahmetov@gmail.com>
2022-05-13 14:38:35 +0300

    fix headers

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const registerHandler = (event) => {
     {
       method: "POST",
       headers: {
-        "Content-Type": "application/json; charset=utf-8",
+        "Content-Type": "application/json;charset=utf-8",
       },
       body: JSON.stringify(userData),
     }
